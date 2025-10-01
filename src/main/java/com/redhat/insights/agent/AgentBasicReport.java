d094546dc798975c7617e9ebc05e23c80a641829
Marko Strukelj <marko.strukelj@gmail.com>
2025-10-01 17:49:25 +0200

    Add subreport extension mechanism
    ServiceLoader based extension mechanism via `com.redhat.insights.agent.api.SubreportProvider`
    
    Signed-off-by: Marko Strukelj <marko.strukelj@gmail.com>

--- src/main/java/com/redhat/insights/agent/AgentBasicReport.java
+++ src/main/java/com/redhat/insights/agent/AgentBasicReport.java
@@ -1,6 +1,7 @@
-/* Copyright (C) Red Hat 2023-2024 */
+/* Copyright (C) Red Hat 2023-2025 */
 package com.redhat.insights.agent;
 
+import com.redhat.insights.agent.api.SubreportProvider;
 import com.redhat.insights.config.InsightsConfiguration;
 import com.redhat.insights.jars.ClasspathJarInfoSubreport;
 import com.redhat.insights.logging.InsightsLogger;
@@ -8,6 +9,7 @@ import com.redhat.insights.reports.AbstractTopLevelReportBase;
 import com.redhat.insights.reports.InsightsSubreport;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ServiceLoader;
 
 public class AgentBasicReport extends AbstractTopLevelReportBase {
   private static final InsightsLogger logger = AgentLogger.getLogger();
@@ -22,7 +24,32 @@ public class AgentBasicReport extends AbstractTopLevelReportBase {
     ClasspathJarInfoSubreport jarsReport = new ClasspathJarInfoSubreport(logger);
     reports.put("jars", jarsReport);
     reports.put("details", AgentSubreport.of(jarsReport, configuration));
+    addExtensionReports(reports, configuration);
     return new AgentBasicReport(configuration, reports);
+  }
+
+  private static void addExtensionReports(
+      Map<String, InsightsSubreport> reports, AgentConfiguration configuration) {
+    // Load the agent SubreportProvider if available
+    ServiceLoader<SubreportProvider> loader = ServiceLoader.load(SubreportProvider.class);
+    for (SubreportProvider provider : loader) {
+      Map<String, InsightsSubreport> subs = provider.provideSubreports(configuration, logger);
+      // add any provided subreports to main agent report
+      // ensure there is no name conflict, fail if there is
+      for (String name : subs.keySet()) {
+        if (reports.containsKey(name)) {
+          throw new RuntimeException(
+              "The key provided by the subreport provider ('"
+                  + name
+                  + "': "
+                  + subs.get(name)
+                  + ") exists already! ("
+                  + reports.get(name)
+                  + ")");
+        }
+        reports.put(name, subs.get(name));
+      }
+    }
   }
 
   @Override
